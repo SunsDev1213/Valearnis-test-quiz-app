a8d5acf07a233f1d3db2ce5798e570f9f26af6ea
suns <suns.dev1213@gmail.com>
2023-01-31 05:12:24 +0300

    updated router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Switch,
+  Route,
+} from "react-router-dom";
 import "./App.css";
 import Quiz from "./views/Quiz";
 import Login from "./views/Auth/Login";
@@ -8,16 +13,16 @@ function App() {
   return (
     <div>
       <Router>
-        {localStorage.token ? (
-          <Switch>
-            <Route exact path="/quiz" component={Quiz} />
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/login" />
+          </Route>
+          <Route exact path="/login" component={Login} />
+          {localStorage.token && <Route exact path="/quiz" component={Quiz} />}
+          {localStorage.token && (
             <Route exact path="/final" component={Final} />
-          </Switch>
-        ) : (
-          <Switch>
-            <Route exact path="/" component={Login} />
-          </Switch>
-        )}
+          )}
+        </Switch>
       </Router>
     </div>
   );
